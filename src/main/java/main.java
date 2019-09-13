6fb9d124fa291879ce1d6980c70e55fac1b6f072
phongtiger <phongken94@gmail.com>
2019-09-13 11:05:57 +0700

    tailen

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0,"orange");
+        System.out.println(circle);
+        Cylinder cylinder = new Cylinder(7.8,"black",20.0);
+        System.out.println(cylinder);
+    }
+}
